de92e25cc6a87d11b3e7392099f280f62c611ccb
Llewellyn Falco <llewellyn.falco@gmail.com>
2022-10-07 11:34:28 -0600

    F!! working verify method
    Co-Authored-By: Matt Hughes <221921+mch@users.noreply.github.com>

--- test/test.ts
+++ test/test.ts
@@ -1,8 +1,34 @@
 import {describe, expect, test} from '@jest/globals';
+import path from "path";
+const approvalsConfig = {
+  reporters: ['beyondcompare', 'kdiff3', 'vscode'],
 
+  normalizeLineEndingsTo: '\n',
 
+  appendEOL: true,
+
+  EOL: "\n",
+
+  errorOnStaleApprovedFiles: true,
+
+  shouldIgnoreStaleApprovedFile: function (/*fileName*/) { return false; },
+
+  stripBOM: true,
+
+  forceApproveAll: false
+}
+const approvals = require('approvals').configure(approvalsConfig);
+
+const basePath = path.join(process.cwd(), 'test/');
+
+function verify(name:string, situationUnderTest:()=>any) {
+  test(name, function() {
+    const result = situationUnderTest();
+    approvals.verify(basePath, name, JSON.stringify(result))
+  });
+}
 describe("something", () => {
-  test("does something", () => {
-    expect(true).toBe(true);
+  verify("does something", () => {
+    return {name:"fred", age: 30}
   });
 });
