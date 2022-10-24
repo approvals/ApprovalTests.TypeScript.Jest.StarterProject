bb9eaf456f88643e7f8b09ba1cb969a7f7e0850b
Llewellyn Falco <llewellyn.falco@gmail.com>
2022-10-24 08:03:01 +0200

    t cleanup examples

--- test/test.ts
+++ test/test.ts
@@ -1,9 +1,10 @@
 import { describe, expect, test } from "@jest/globals";
-import {verifyAsJson} from "approvals/lib/Providers/Jest/JestApprovals";
+import {verify, verifyAsJson} from "approvals/lib/Providers/Jest/JestApprovals";
+
 
 describe("ApprovalTests", () => {
    test("SimpleVerify", () => {
-     verifyAsJson("Hello From Approvals");
+     verify("Hello From Approvals");
    });
   test("JsonVerify", () => {
     const data = {name:"fred", age: 30}
