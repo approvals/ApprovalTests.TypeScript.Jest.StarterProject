5926bfc7ffa38e02aa6571920a6c1b052d803b14
jfgreffier <jfgreffier@users.noreply.github.com>
2023-08-20 22:31:11 +0200

    F add JestReporter usage to template

--- test/Sample.test.ts
+++ test/Sample.test.ts
@@ -1,8 +1,15 @@
-import { describe, expect, test } from "@jest/globals";
+import { beforeAll, describe, expect, test } from "@jest/globals";
+import { configure } from "approvals";
+import { JestReporter } from "approvals/lib/Providers/Jest/JestReporter";
 import {verify, verifyAsJson} from "approvals/lib/Providers/Jest/JestApprovals";
 
 
 describe("ApprovalTests", () => {
+  beforeAll(() => {
+    configure({
+      reporters: [new JestReporter()],
+    });
+  });
    test("SimpleVerify", () => {
      verify("Hello From Approvals");
    });
